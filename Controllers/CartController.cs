817cf58fc48e0ca0cc968ad046f059c323b1710e
turkialothman <turkialothman4@gmail.com>
2024-07-23 07:07:59 +0300

    changes routing system

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,209 +1,27 @@
-
-using e_commerce.Domain.Models;
-using e_commerce.Domain.Repositories.Interfaces;
-using e_commerce.Services;
-using e_commerce.utils;
-using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
 namespace e_commerce.Controllers
 {
-    [Authorize]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CartController : ControllerBase
+    [Route("[controller]")]
+    public class CartController : Controller
     {
+        private readonly ILogger<CartController> _logger;
 
-        private readonly IProductRepository _ProductRepository;
-        private readonly ICartRepository _CartRepository;
-        private readonly JwtService jwt;
-        bool signedIn ;
-        string UserId ;
-        public CartController(IProductRepository productRepository,ICartRepository CartRepository, JwtService jwt , IHttpContextAccessor context){
-            
-            this._ProductRepository = productRepository;
-            this._CartRepository = CartRepository;
-            this.jwt = jwt;
-            signedIn =  bool.Parse(context.HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == "signed_in")!.Value);
-            UserId = context.HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == "id")!.Value;
+        public CartController(ILogger<CartController> logger)
+        {
+            _logger = logger;
         }
 
-
-        
-        [HttpPost("{ProductId}")]
-        public async Task<IActionResult> AddItemToCart(string ProductId){
-            
-            Product product =  await _ProductRepository.getById(ProductId);
-
-            
-            if (product == null)
-                return NotFound(new {message = "product not found"});
-                       
-            List<ProductItem> items;
-            Cart cart = null;
-            
-            // get the cart from database if authenticated(signed_in) otherwise get from session
-
-            if(!signedIn)
-            {
-                string productString = HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == "products")!.Value;
-                items = JsonConvert.DeserializeObject<List<ProductItem>>(productString);
-            }
-            else
-            {
-                cart = await this._CartRepository.GetCart(UserId);
-                items = cart.products;
-            }
-
-
-                bool found =false;
-                foreach (ProductItem item in items)
-                {
-                    if(item.ProductId == product.Id)
-                    {
-                        if(item.Quentity + 1 > product.Quantity)
-                            return UnprocessableEntity(new {message ="Insufficient product quantity available"});
-                        found = true;
-                        item.Quentity +=1;
-                    }                        
-                            
-                }
-                
-                if(!found)
-                    items.Add(new ProductItem{ProductId = product.Id ,Quentity = 1});
-                
-                
-                // update cart in database if authenticated(signed_in) otherwise update in session
-                if (!signedIn)
-                {
-                    string token = jwt.GenerateToken(products:items.ToArray());
-                    HttpContext.Response.Cookies.Append("jwt",token);                    
-                }
-                else
-                {
-                    await this._CartRepository.update(cart);
-                }
-
-            
-            
-            return Ok(new {message = "updates successfully"});
-            
-        } 
-
-        
-        
-        
-        
-        [HttpGet]
-        public async Task<ActionResult> GetCart(){
-            List<ProductItem> items;
-            Cart cart = null;
-            
-            if(!signedIn)
-                items =  JsonConvert.DeserializeObject<List<ProductItem>>(HttpContext.User.Claims.FirstOrDefault(c => c.Type == "products")!.Value);
-            else{
-                cart = await this._CartRepository.GetCart(UserId);
-                items = cart.products;
-            }
-            
-            return Ok(items);
-        }
-    
-        
-        
-        
-        
-        [HttpDelete("{ProductId}")]
-        public async Task<ActionResult> removeItem(string ProductId){
-            
-            Product product =  await _ProductRepository.getById(ProductId);
-            
-            
-            if (product == null)
-                return NotFound(new {message = "product not found"});
-
-            List<ProductItem> items;
-            Cart cart = null;
-            
-            if(!signedIn)
-            {
-                string itemsString = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "products")!.Value;
-                items = JsonConvert.DeserializeObject<List<ProductItem>>(itemsString)!;
-            }
-            else
-            {
-                cart = await this._CartRepository.GetCart(UserId);
-                items = cart.products;
-            }
-            
-            
-            for (int i = 0; i < items.Count(); i++)
-            {
-                if(items[i].ProductId == product.Id)
-                    items[i].Quentity -= 1;
-                
-                if(items[i].Quentity == 0)
-                    items.RemoveAt(i);
-            }
-
-            if (!signedIn)
-            {
-                string token = jwt.GenerateToken(products:items.ToArray());
-                HttpContext.Response.Cookies.Append("jwt",token);    
-            }
-            else
-            {
-                await this._CartRepository.update(cart);
-            }
-            
-
-            return Ok(new {message = "item removed successfully"});
+        public IActionResult Index()
+        {
+            return View();
         }
 
-        
-
-
-        [HttpDelete("remove/{ProductId}")]
-        public async Task<ActionResult> removeProduct(string ProductId){
-            
-            Product product =  await _ProductRepository.getById(ProductId);
-            
-            if (product == null)
-                return NotFound(new {message = "product not found"});
-
-            List<ProductItem> items;
-            Cart cart = null;
-            
-            if(!signedIn)
-            {
-                string itemsString = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "products")!.Value;
-                items = JsonConvert.DeserializeObject<List<ProductItem>>(itemsString)!;
-            }
-            else
-            {
-                cart = await this._CartRepository.GetCart(UserId);
-                items = cart.products;
-            }
-
-          for (int i = 0; i < items.Count(); i++)
-          {
-            if (items[i].ProductId == product.Id)
-                items.RemoveAt(i);
-          }
-
-            if(!signedIn)
-            {
-                string token = jwt.GenerateToken(products:items.ToArray());
-                HttpContext.Response.Cookies.Append("jwt",token);    
-            }
-            else
-            {
-                await this._CartRepository.update(cart);
-            }
-
-            return Ok(new {message = "Product removed successfully"});
-        }
-
-    
     }
 }
