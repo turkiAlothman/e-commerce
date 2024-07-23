0d1abf1cae2304cd2a5042f1af5722dd2574e961
turkialothman <turkialothman4@gmail.com>
2024-07-23 22:59:06 +0300

    change home controller

--- Controllers/HomeCntroller.cs
+++ Controllers/HomeCntroller.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
-namespace e_commerce.Controllers.REST
+namespace e_commerce.Controllers
 {
     [Route("[controller]")]
     public class HomeCntroller : Controller
     {
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpGet]
         public IActionResult Error()
         {
             return View("Error!");
