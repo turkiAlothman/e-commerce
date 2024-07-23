5c08dc15310c627d190ccf01242edcd68ab45452
turkialothman <turkialothman4@gmail.com>
2024-07-23 22:20:33 +0300

    add erre middleware

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -10,12 +10,10 @@ namespace e_commerce.Controllers;
 [Authorize]
 public class ProductsController : Controller
 {
-    private readonly ILogger<ProductsController> _logger;
     private readonly IProductRepository _productRepository;
 
-    public ProductsController(ILogger<ProductsController> logger,IProductRepository _productRepository)
+    public ProductsController(IProductRepository _productRepository)
     {
-        this._logger = logger;
         this._productRepository = _productRepository;
     }
 
@@ -36,10 +34,4 @@ public class ProductsController : Controller
         return View();
     }
 
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
---
+++ Controllers/REST/HomeCntroller.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace e_commerce.Controllers.REST
+{
+    [Route("[controller]")]
+    public class HomeCntroller : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
